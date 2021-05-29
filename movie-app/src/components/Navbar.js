adc3b6d79ad3b5e6a5511edc56d75178a224da9a
THANOS_CAR <jan.342003@seznam.cz>
2021-05-29 14:18:49 +0200

    styled navbar

--- movie-app/src/components/Navbar.js
+++ movie-app/src/components/Navbar.js
@@ -1,15 +1,19 @@
 const Navbar = () => {
     return (
-        <div>
-            <div className="brand">
-                <h1>Movie App</h1>
-            </div>
+        <div className="navbar">
+            <div className="navbar-container">
+                
+                <div className="brand">
+                    <h1>Movie App</h1>
+                </div>
 
-            <div className="search">
-                <form action="submit">
-                    <input type="text" placeholder="movie..." />
-                    <button type="submit">search</button>
-                </form>
+                <div className="search">
+                    <form action="submit">
+                        <input type="text" placeholder="movie..." />
+                        <button type="submit">search</button>
+                    </form>
+                </div>
+
             </div>
         </div>
     );
